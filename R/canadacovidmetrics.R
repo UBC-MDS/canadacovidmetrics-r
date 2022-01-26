3628d0fba2a0fc5ff03f6b299c5b8f9180563efe
Brandon Lam <ming0701@gmail.com>
2022-01-25 23:00:26 -0800

    bug fixed

--- R/canadacovidmetrics.R
+++ R/canadacovidmetrics.R
@@ -90,32 +90,30 @@ total_cumulative_cases <- function(loc='prov', date=NULL, after='2020-01-01', be
 #' total_cumulative_deaths(loc = "ON", before = "2021-12-31")
 #' total_cumulative_deaths(loc = "prov", date = "2021-09-01")
 
-total_cumulative_deaths <- function(loc='prov', date=NULL, after='2020-01-01', before=Sys.Date(), datetime_type=FALSE){}
-
+total_cumulative_deaths <- function(loc='prov', date=NULL, after='2020-01-01', before=Sys.Date(), datetime_type=FALSE){
+  
   loc_format_check(loc)
-
-  if(date != NULL){
-    date_format_check(date)  # check date is valid
-    url = paste0('https://api.opencovid.ca/timeseries?stat=mortality&loc=', loc , '&date=', date)
-  }else{
-
-    date_format_check(before)  # check before-date is valid
-    date_format_check(after)  # check after-date is valid
-    url = paste0('https://api.opencovid.ca/timeseries?stat=mortality&loc=', loc , '&after=', after, '&before=', before)
+  
+  if (!is.null(date)) {
+    date_format_check(date)
+    url <- sprintf("https://api.opencovid.ca/timeseries?stat=mortality&loc=%s&date=%s", loc, date)
+  } else {
+    date_format_check(after)
+    date_format_check(before)
+    url <- sprintf("https://api.opencovid.ca/timeseries?stat=mortality&loc=%s&after=%s&before=%s", loc, after, before)
   }
-
-  # Get Json Object
-  jsonData = GET(url)
-  json_body = rawToChar(jsonData$content)
-  df = fromJSON(json_body)$mortality
+  
+  raw_json <- GET(url)
+  json_string <- content(raw_json, as="text")
+  df <- fromJSON(json_string)$mortality
 
   if(datetime_type){
-    df$date_death_report = as.Date(df$date_death_report, format = "%d-%m-%y")
+    df$date_vaccine_completed = as.Date(df$date_vaccine_completed, format = "%d-%m-%y")
   }
-
-  return df
-
+    
+  return(df)
 }
+
 
 #' Query total cumulative recovered cases with ability to specify
 #' location and date range of returned data.
